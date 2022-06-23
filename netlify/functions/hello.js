0298f025b08360c36176301fc92575d4c8824ad7
Giffen-good <chris6rock@gmail.com>
2022-06-23 16:24:17 -0400

    CORS Enabled

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -7,9 +7,15 @@
 
 
 exports.handler = async event => {
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+    };
     const subject = event.queryStringParameters.name || 'World'
     return {
         statusCode: 200,
+        headers,
         body: `Hello ${subject}!`,
     }
 }
