647c268bdd2b7027893782200d3014b3e8027677
Giffen-good <chris6rock@gmail.com>
2022-06-23 19:17:41 -0400

    commonJS

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,47 +1,53 @@
-// hello there!
-// 
-// I'm a serverless function that you can deploy as part of your site.
-// I'll get deployed to AWS Lambda, but you don't need to know that. 
-// You can develop and deploy serverless functions right here as part
-// of your site. Netlify Functions will handle the rest for you.
-import fetch from 'node-fetch'
-export const handler = async event => {
-    const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-    };
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const axios = require('axios').default;
+
+const handler = async (event) => {
+    try {
+        const headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+        };
 
 
 
-    const phone = event.queryStringParameters.phone || 'xxxx'
-    const email = event.queryStringParameters.email || 'xxxx'
+        const phone = event.queryStringParameters.phone || 'xxxx'
+        const email = event.queryStringParameters.email || 'xxxx'
 
-    const options = {
-        method: 'POST',
-        headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            profiles: [{email: email}, {phone_number: phone, sms_consent: true}]
-        })
-    };
-    const response = await fetch(`https://a.klaviyo.com/api/v2/list/Wiw2cn/subscribe?api_key=${process.env.PRIVATE_KEY}`, options)
-    const data = await response.json()
-    console.log(response)
-    console.log(`\n`)
-    console.log(data)
-    if (data?.detail) {
-        return {
+        const options = {
+            method: 'POST',
+            headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                profiles: [{email: email}, {phone_number: phone, sms_consent: true}]
+            })
+        };
+        const response = await axios.post(`https://a.klaviyo.com/api/v2/list/Wiw2cn/subscribe?api_key=${process.env.PRIVATE_KEY}`, options)
+        const data = await response.json()
+        console.log(response)
+        console.log(`\n`)
+        console.log(data)
+        if (data?.detail) {
+          return {
             statusCode: 400,
             headers,
             body: JSON.stringify({res: data.detail})
-        }
-    } else {
-        return {
+          }
+        } else {
+          return {
             statusCode: 200,
             headers,
             body: JSON.stringify({res: "success"})
+          }
+
         }
-
+        // return {
+        //     statusCode: 200,
+        //     headers,
+        //     body: JSON.stringify({res: "success"})
+        // }
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
     }
-
 }
+
+module.exports = { handler }
--- netlify/functions/setPhoneNumber/setPhoneNumber.js
+++ netlify/functions/setPhoneNumber/setPhoneNumber.js
@@ -1,50 +1,14 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-import fetch from 'node-fetch'
-
 const handler = async (event) => {
   try {
-    const headers = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-    };
-
-
-
-    const phone = event.queryStringParameters.phone || 'xxxx'
-    const email = event.queryStringParameters.email || 'xxxx'
-
-    const options = {
-      method: 'POST',
-      headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        profiles: [{email: email}, {phone_number: phone, sms_consent: true}]
-      })
-    };
-    const response = await fetch(`https://a.klaviyo.com/api/v2/list/Wiw2cn/subscribe?api_key=${process.env.PRIVATE_KEY}`, options)
-    const data = await response.json()
-    // console.log(response)
-    // console.log(`\n`)
-    // console.log(data)
-    // if (data?.detail) {
-    //   return {
-    //     statusCode: 400,
-    //     headers,
-    //     body: JSON.stringify({res: data.detail})
-    //   }
-    // } else {
-    //   return {
-    //     statusCode: 200,
-    //     headers,
-    //     body: JSON.stringify({res: "success"})
-    //   }
-    //
-    // }
+    const subject = event.queryStringParameters.name || 'World'
     return {
-          statusCode: 200,
-          headers,
-          body: JSON.stringify({res: "success"})
-        }
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
