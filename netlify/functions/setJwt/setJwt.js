b894fd0d7c16f852b98e68590cf3224ad1b9d133
Giffen-good <chris6rock@gmail.com>
2022-07-25 16:01:47 -0400

    add logs

--- netlify/functions/setJwt/setJwt.js
+++ netlify/functions/setJwt/setJwt.js
@@ -166,7 +166,7 @@ const getStorefrontToken = async (username, password) => {
   const { data } = response
   console.log(`Domain:: ${process.env.SHOP_DOMAIN}`)
   console.log(`storefront token:: ${process.env.STOREFRONT_ACCESS_TOKEN}`)
-  console.log(data.data)
+  console.log(data.data.customerAccessTokenCreate.customerUserErrors[0])
   if (!data.data.customerAccessTokenCreate.customerAccessToken)
     throw "Error: Failed to retrieve access token"
   return data.data.customerAccessTokenCreate.customerAccessToken.accessToken;
